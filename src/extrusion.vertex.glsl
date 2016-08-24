e55499082c1bf0ce927d044f793bc73e284bcc91
Nicki Dlugash <nicki@mapbox.com>
2016-08-24 19:33:53 -0400

    extrusion vertex: removed extra ambient light from starting color value, updated starting directional, updated adjusted directional to account for update to starting directional, simplified logic for gradient

--- src/extrusion.vertex.glsl
+++ src/extrusion.vertex.glsl
@@ -36,29 +36,35 @@ void main() {
     // How dark/bright is the surface color?
     float colorvalue = (color.r + color.g + color.b) / 3.0;
 
+    v_color = vec4(0.0, 0.0, 0.0, 1.0);
+
     // Add slight ambient lighting so no extrusions are totally black
     // TODO: include the lightintensity in the calculation?
     vec4 ambientlight = vec4(0.03, 0.03, 0.03, 1.0);
-    v_color = ambientlight;
     color += ambientlight;
 
     // Calculate cos(theta), where theta is the angle between surface normal and diffuse light ray 
-    float directional = clamp(dot(a_normal / 32768.0, u_lightdir), 0.0, 1.0);
+    //float directional = clamp(dot(a_normal / 32768.0, u_lightdir), 0.0, 1.0);
+    float directional = clamp(dot(a_normal / 16384.0, u_lightdir), 0.0, 1.0);
 
     // Adjust directional so that 
     // the range of values for highlight/shading is narrower 
     // with lower light intensity
     // and with lighter/brighter surface colors
-    directional = mix((1.0 - lightintensity), max((2.0 - colorvalue + lightintensity), 1.0), directional);
+    //directional = mix((1.0 - lightintensity), max((2.0 - colorvalue + lightintensity), 1.0), directional);
+    directional = mix((1.0 - lightintensity), max((1.0 - colorvalue + lightintensity), 1.0), directional);   
 
     // Add gradient along z axis of side surfaces
     // Still needs a bit of work before usable
     float t = mod(a_normal.x, 2.0);
     // t = 1 when top vertex, t = 0 when bottom vertex
     if (a_normal.y != 0.0) {
-        directional = mix(0.0, 1.0, directional * 
-            ((0.2 + clamp(pow(t * (maxH / 150.0), 0.75), 0.7, 1.0)) / 1.2));
+        //directional = mix(0.0, 1.0, directional * 
+            //((0.2 + clamp(pow(t * (maxH / 150.0), 0.75), 0.7, 1.0)) / 1.2));
+    directional *= clamp(t * pow(maxH / 150.0, 0.5), mix(0.7, 0.98, 1.0 - lightintensity), 1.0);
     }
+
+
 
     // Assign final color based on surface + ambient light color, diffuse light directional, and light color
     // with lower bounds adjusted to hue of light
